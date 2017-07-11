d571a568d998c279b0e5643cf7dc3b943f8d21f2
Andres TheFirst <darkshadowskull@gmail.com>
2017-07-10 20:40:31 -0500

    Emit message on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 socket.on('connect', function() {
   console.log('connected to server');
 
-  socket.emit('createMessage', {
-    from: "seiya",
-    text: "Not much"
-  });
-
 });
 
 socket.on('disconnect', function() {
--- server/server.js
+++ server/server.js
@@ -14,14 +14,14 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: "iki",
-    text: "Sup man",
-    createdAt: 033
-  });
-
   socket.on('createMessage', (message) => {
     console.log("Got new messge", message);
+
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      cretedAt: new Date().getTime()
+    });
   });
 
   socket.on('disconnect', () => {
