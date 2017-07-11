74a404e37cc7809ab504d3a922874c0121aef800
Andres TheFirst <darkshadowskull@gmail.com>
2017-07-11 00:35:54 -0500

    Add geolocation support via geolocation api

--- public/js/index.js
+++ public/js/index.js
@@ -15,6 +15,16 @@ socket.on('newMessage', function(message) {
     $('#messages').append(li);
 });
 
+socket.on('newLocationMessage', (message) => {
+  var li = $('<li></li>');
+  var a = $('<a target="_blanck">My current location</a>')
+
+  li.text(message.from + ': ');
+  a.attr('href', message.url);
+  li.append(a);
+  $('#messages').append(li);
+});
+
 $('#message-form').on('submit', function(e) {
   e.preventDefault();
 
@@ -24,4 +34,20 @@ $('#message-form').on('submit', function(e) {
   }, function() {
 
   });
+});
+
+var locationButton = $('#send-location');
+locationButton.on('click', function() {
+  if (!navigator.geolocation) {
+    return alert('geolocation not supported by your browser');
+  }
+
+  navigator.geolocation.getCurrentPosition(function(position) {
+    socket.emit('createLocationMessage', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    });
+  }, function() {
+    alert('Unable to fetch location.');
+  })
 });
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
@@ -10,4 +10,16 @@ describe('generateMessage', () => {
     expect(rest).toInclude({from, text});
     expect(rest.createdAt).toBeA('number');
   });
+});
+
+describe('generateLocationMessage', () => {
+    it('should generate correct location object', () => {
+      var from = 'Ron';
+      var latitude = '6';
+      var longitude = '3';
+      var url = 'http://www.google.com/maps?q=6,3';
+      var rest = generateLocationMessage(from, latitude, longitude);
+      expect(rest).toInclude({from});
+      expect(rest.createdAt).toBeA('number');
+    });
 });
