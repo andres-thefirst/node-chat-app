b673f6d059d5473db401359e67082c94d99aa64d
Andres TheFirst <darkshadowskull@gmail.com>
2017-07-12 21:16:31 -0500

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,19 @@
 var socket = io();
+
+function scrollToBottom() {
+  var messages = $('#messages');
+  var newMessage = messages.children('li:last-child');
+
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+  if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect', function() {
   console.log('connected to server');
 });
@@ -17,7 +32,7 @@ socket.on('newMessage', function(message) {
     });
 
     $('#messages').append(html);
-
+    scrollToBottom();
     // var li = $('<li></li>');
     // li.text(`${message.from}  ${formatttedTime}: ${message.text}`);
     // $('#messages').append(li);
@@ -33,6 +48,7 @@ socket.on('newLocationMessage', (message) => {
   });
 
   $('#messages').append(html);
+  scrollToBottom();
   // var li = $('<li></li>');
   // var a = $('<a target="_blanck">My current location</a>')
   // var formatttedTime = moment(message.createdAt).format('h:mm a');
